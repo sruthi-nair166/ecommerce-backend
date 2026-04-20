a27e907b8f440bb5e9199633004ea3f52e75e63b
Sruthi <sruthivnair78@gmail.com>
2026-04-20 12:04:16 +0530

    Initial setup: project structure and basic server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server running");
+});
